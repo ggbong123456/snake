f870e0e54b9c07f98913cf9b68ece48a7e7cba9c
ggbong123456 <134274478+ggbong123456@users.noreply.github.com>
2023-06-18 12:59:37 +0800

    Update state.c

--- state.c
+++ state.c
@@ -7,6 +7,13 @@
 #define BOARD_ROW (10)
 #define BOARD_COL (10)
 
+static char get_board_at(game_state_t* state,int x,int y){
+ return state->board[x][y]; 
+}
+
+static void set_board_at(game_state_t* state,int x,int y,char ch){
+ state->board[x][y]=ch; 
+}
 game_state_t* creat_default_state(){
   game_state_t* default_state=(game_state_t*)malloc(sizeof(game_state_t));
   default_state->x_size=BOARD_ROW;
@@ -23,5 +30,19 @@ game_state_t* creat_default_state(){
       memset(default_state->board[i]+BOARD_COL-1,'#',sizeof(char));
     }
   }
+  set_board_at(game_state_t* state,2,9,'*');
+  default_state->num_snakes=1;
+  default_state->snakes=(snake_t*)malloc(sizeof(snake_t));
+  default_state->snakes->tail_x=4;
+  default_state->snakes->tail_y=4;
+  default_state->snakes->head_x=4;
+  default_state->snakes->head_y=4;
+  default_state->snakes->live = true;
+  set_board_at(game_state_t* state,default_state->snakes->tail_x,default_state->snakes->tail_y,'d');
+  set_board_at(game_state_t* state,default_state->snakes->head_x,default_state->snakes->head_y,'>');
+  return default_state;
+  
+}
+  
   
   
